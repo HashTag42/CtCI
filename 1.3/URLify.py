965f165553fea7455d1867031d53a9d2f8531268
César García <ceegee@outlook.com>
2025-10-08 11:59:04 -0700

    Add docstring to the function

--- 1.3/URLify.py
+++ 1.3/URLify.py
@@ -1,8 +1,8 @@
 '''
 Cracking the Coding Interview - Problem 1.3
     URLify: Write a method to replace all spaces in a string with '%20'. You may assume that the string has
-        sufficient space at the end to hold the additional characters, and that you're given the "true"
-        length of the string.
+    sufficient space at the end to hold the additional characters, and that you're given the "true" length
+    of the string.
 
     EXAMPLE
         Input: "Mr John Smith      ", 13
@@ -11,6 +11,16 @@ Cracking the Coding Interview - Problem 1.3
 
 
 def URLify(string: str, true_length: int) -> str:
+    """
+    Replaces spaces in a string with its URL-encoded representation: %20
+
+    Args:
+        string (str) : The source string containing spaces
+        true_length (int): The "true" length of the string.
+
+    Returns:
+        str : The resulting string.
+    """
     result = ""
     for i in range(true_length):
         if string[i] == ' ':
