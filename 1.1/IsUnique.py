16e48fadc7c56fd56f75157ce13ef0a4da7e1ff6
César García <ceegee@outlook.com>
2025-10-07 19:20:08 -0700

    Rename function to IsUnique2 and update test cases for consistency

--- 1.1/IsUnique.py
+++ 1.1/IsUnique.py
@@ -5,7 +5,7 @@ Cracking the Coding Interview - Problem 1.1
 '''
 
 
-def IsUnique(s: str) -> bool:
+def IsUnique1(s: str) -> bool:
     """
     Checks if a string has all unique characters.
 
@@ -14,6 +14,11 @@ def IsUnique(s: str) -> bool:
 
     Returns:
         bool: True if the string has all unique characters. False otherwise.
+
+    Description:
+        Implementation uses a dictionary to track which characters have been found as we scan the string.
+        Time complexity: O(N)
+        Space complexity: O(N)
     """
     instances = {}
     for c in s:
@@ -22,4 +27,26 @@ def IsUnique(s: str) -> bool:
             instances[c] = 1
         else:
             return False
+    return True
+
+
+def IsUnique2(s: str) -> bool:
+    """
+    Checks if a string has all unique characters.
+
+    Args:
+        s (str) : The string to check.
+
+    Returns:
+        bool: True if the string has all unique characters. False otherwise.
+
+    Description:
+        Implementation does not use any additional data structures. It scans the string twice to find duplicates.
+        Time complexity: O(N^2)
+        Space complexity: O(1)
+    """
+    for i in range(len(s)):
+        for j in range(i + 1, len(s)):
+            if s[j] == s[i]:
+                return False
     return True
--- 1.1/test_isUnique.py
+++ 1.1/test_isUnique.py
@@ -1,17 +1,22 @@
-from IsUnique2 import IsUnique
+from IsUnique import IsUnique1
+from IsUnique import IsUnique2
 
 
-def test_case_1():
-    assert IsUnique("ab") is True
+def test_IsUnique_1():
+    assert IsUnique1("ab") is True
+    assert IsUnique2("ab") is True
 
 
-def test_case_2():
-    assert IsUnique("aa") is False
+def test_IsUnique_2():
+    assert IsUnique1("aa") is False
+    assert IsUnique2("aa") is False
 
 
-def test_case_3():
-    assert IsUnique("") is True
+def test_IsUnique_3():
+    assert IsUnique1("") is True
+    assert IsUnique2("") is True
 
 
-def test_case_4():
-    assert IsUnique("abcdef0123456ABCDEFáñ☺️東京") is True
+def test_IsUnique_4():
+    assert IsUnique1("abcdef0123456ABCDEFáñ☺️東京") is True
+    assert IsUnique2("abcdef0123456ABCDEFáñ☺️東京") is True
