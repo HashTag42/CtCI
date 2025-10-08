3724445b0451a8bdf4e713cd3cf82115971bb5c0
César García <ceegee@outlook.com>
2025-10-08 15:57:15 -0700

    Refactor unit tests using a fixture

--- 1.1/test_isUnique.py
+++ 1.1/test_isUnique.py
@@ -1,34 +1,25 @@
 from IsUnique import IsUnique1
 from IsUnique import IsUnique2
+import pytest
 
 
-def test_IsUnique1_1():
-    assert IsUnique1("ab") is True
+@pytest.fixture(params=[
+        ("ab", True),
+        ("aa", False),
+        ("", True),
+        ("abcdef0123456ABCDEFáñ☺️東京", True),
+])
+def test_case(request):
+    return request.param
 
 
-def test_IsUnique1_2():
-    assert IsUnique1("aa") is False
+def test_IsUnique1(test_case):
+    input_str, expected = test_case
+    actual = IsUnique1(input_str)
+    assert actual == expected
 
 
-def test_IsUnique1_3():
-    assert IsUnique1("") is True
-
-
-def test_IsUnique1_4():
-    assert IsUnique1("abcdef0123456ABCDEFáñ☺️東京") is True
-
-
-def test_IsUnique2_1():
-    assert IsUnique2("ab") is True
-
-
-def test_IsUnique2_2():
-    assert IsUnique2("aa") is False
-
-
-def test_IsUnique2_3():
-    assert IsUnique2("") is True
-
-
-def test_IsUnique2_4():
-    assert IsUnique2("abcdef0123456ABCDEFáñ☺️東京") is True
+def test_IsUnique2(test_case):
+    input_str, expected = test_case
+    actual = IsUnique2(input_str)
+    assert actual == expected
