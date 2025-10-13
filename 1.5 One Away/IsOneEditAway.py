7e7575f9f874b9a5e0bd73cd85afecded7b89cf8
César García <ceegee@outlook.com>
2025-10-12 19:05:29 -0700

    Removing unnecessary branches and debug output lines. Statement and branch coverage remains at 100%

--- 1.5 One Away/IsOneEditAway.py
+++ 1.5 One Away/IsOneEditAway.py
@@ -15,44 +15,34 @@ pale, bake  -> False
 def IsOneEditAway(string1: str, string2: str) -> bool:
 
     if string1 == string2:
-        print("[DEBUG] 1")
         return True
 
     if abs(len(string1) - len(string2)) >= 2:
-        print("[DEBUG] 2")
         return False
 
     if len(string1) == len(string2):
         # Potential replacement
-        print("[DEBUG] 3")
         return CheckReplacement(string1, string2)
 
     if len(string1) - len(string2) == 1:
         # Potential removal
-        print("[DEBUG] 4")
         return CheckRemoval(string1, string2)
 
-    if len(string1) - len(string2) == -1:   # pragma: no branch
-        # Potential insertion
-        # An insertion is a reversed removal, so we pass the strings to CheckRemoval in reversed order
-        print("[DEBUG] 5")
-        return CheckRemoval(string2, string1)
+    # len(string1) - len(string2) is -1:
+    # Potential insertion
+    # An insertion is a reversed removal, so we pass the strings to CheckRemoval in reversed order
+    return CheckRemoval(string2, string1)
 
 
 def CheckReplacement(string1: str, string2: str) -> bool:
     diff_count = 0
     for i in range(len(string1)):
         if string1[i] != string2[i]:
-            print("[DEBUG] 6")
             diff_count += 1
-        else:
-            print("[DEBUG] 7")
 
     if diff_count <= 1:
-        print("[DEBUG] 8")
         return True
     else:
-        print("[DEBUG] 9")
         return False
 
 
@@ -62,13 +52,8 @@ def CheckRemoval(string1: str, string2: str) -> bool:
     for i in range(min_len):
         if string1[i] != string2[i]:
             if string1[i+1:] == string2[i:]:
-                print("[DEBUG] 10")
                 return True
             else:
-                print("[DEBUG] 11")
                 return False
-        else:
-            print("[DEBUG] 12")
 
-    print("[DEBUG] 13")
     return True
