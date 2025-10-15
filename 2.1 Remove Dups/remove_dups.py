143d1143e9062e8268ad32c41c47946a4fff990f
César García <ceegee@outlook.com>
2025-10-14 18:51:58 -0700

    Solve question #2.1 Remove Dups

---
+++ 2.1 Remove Dups/remove_dups.py
@@ -0,0 +1,44 @@
+'''
+    Cracking the Coding Interview - 6th Edition
+    Question 2.1 - Remove Dups
+    Write code to remove duplicates from an unsorted linked list.
+    FOLLOW UP
+    How would you solve this problem if a temporary buffer is now allowed?
+'''
+from typing import List
+from LinkedList import LinkedList
+
+
+def remove_dups1(linked_list: List[int]) -> List[int]:
+    """
+        Removes duplicated items from an unsorted linked list.
+
+        Args:
+            linked_list (list): the source LinkedList
+
+        Returns:
+            (list): a new LinkedList with the duplicates removed
+    """
+    ll, ll2 = LinkedList(), LinkedList()
+    ll.append_from_list(linked_list)
+    s = set()
+    for val in ll:
+        if val not in s:
+            s.add(val)
+            ll2.append(val)
+    return ll2.to_list()
+
+
+def remove_dups2(linked_list: List[int]) -> List[int]:
+    ll = LinkedList()
+    ll.append_from_list(linked_list)
+    seen = set()
+    current = ll.head
+    while current:
+        if current.data in seen:
+            # ll.delete(current.data)
+
+        else:
+            seen.add(current.data)
+        current = current.next
+    return ll.to_list()
---
+++ 2.1 Remove Dups/test_remove_dups.py
@@ -0,0 +1,36 @@
+from remove_dups import remove_dups1, remove_dups2
+import pytest
+
+
+@pytest.fixture(params=[
+    ([], []),                                           # Empty list
+    ([1], [1]),                                         # One item, no dup
+    ([1, 1], [1]),                                      # One dup
+    ([1, 2, 3, 2], [1, 2, 3]),                          # One dup
+    ([1, 2, 3, 1, 4, 1], [1, 2, 3, 4]),                 # Two dups
+    (["A", "B", "C", "D"], ["A", "B", "C", "D"]),       # No dups
+])
+def test_case1(request: pytest.FixtureRequest):
+    return request.param
+
+
+def test_remove_dups1(test_case1):
+    input, expected = test_case1
+    assert expected == remove_dups1(input)
+
+
+@pytest.fixture(params=[
+    ([], []),                                           # Empty list
+    ([1], [1]),                                         # One item, no dup
+    ([1, 1], [1]),                                      # One dup
+    ([1, 2, 3, 2], [1, 3, 2]),                          # One dup
+    ([1, 2, 3, 1, 4, 1], [2, 3, 4, 1]),                 # Two dups
+    (["A", "B", "C", "D"], ["A", "B", "C", "D"]),       # No dups
+])
+def test_case2(request: pytest.FixtureRequest):
+    return request.param
+
+
+def test_remove_dups2(test_case2):
+    input, expected = test_case2
+    assert expected == remove_dups2(input)
