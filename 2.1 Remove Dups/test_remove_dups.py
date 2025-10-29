6e2d853b31a7c26b90280fed6aa8b1cb562b7159
César García <ceegee@outlook.com>
2025-10-29 15:29:14 -0700

    Refactor test cases

--- 2.1 Remove Dups/test_remove_dups.py
+++ 2.1 Remove Dups/test_remove_dups.py
@@ -2,35 +2,34 @@ from remove_dups import remove_dups1, remove_dups2
 import pytest
 
 
-@pytest.fixture(params=[
+common_cases = [
+    # linked_list, expected
     ([], []),                                           # Empty list
     ([1], [1]),                                         # One item, no dup
     ([1, 1], [1]),                                      # One dup
+    (["A", "B", "C", "D"], ["A", "B", "C", "D"]),       # No dups
+]
+
+
+cases_remove_dups1 = [
+    # linked_list, expected
     ([1, 2, 3, 2], [1, 3, 2]),                          # One dup
     ([1, 2, 3, 1, 4, 1], [2, 3, 4, 1]),                 # Two dups
-    (["A", "B", "C", "D"], ["A", "B", "C", "D"]),       # No dups
-])
-def test_case1(request: pytest.FixtureRequest):
-    return request.param
+]
 
 
-def test_remove_dups1(test_case1):
-    input, expected = test_case1
-    assert expected == remove_dups1(input)
-
-
-@pytest.fixture(params=[
-    ([], []),                                           # Empty list
-    ([1], [1]),                                         # One item, no dup
-    ([1, 1], [1]),                                      # One dup
+cases_remove_dups2 = [
+    # linked_list, expected
     ([1, 2, 3, 2], [1, 2, 3]),                          # One dup
     ([1, 2, 3, 1, 4, 1], [1, 2, 3, 4]),                 # Two dups
-    (["A", "B", "C", "D"], ["A", "B", "C", "D"]),       # No dups
-])
-def test_case2(request: pytest.FixtureRequest):
-    return request.param
+]
 
 
-def test_remove_dups2(test_case2):
-    input, expected = test_case2
-    assert expected == remove_dups2(input)
+@pytest.mark.parametrize("linked_list, expected", common_cases + cases_remove_dups1)
+def test_remove_dups1(linked_list, expected):
+    assert remove_dups1(linked_list) == expected
+
+
+@pytest.mark.parametrize("linked_list, expected", common_cases + cases_remove_dups2)
+def test_remove_dups2(linked_list, expected):
+    assert remove_dups2(linked_list) == expected
