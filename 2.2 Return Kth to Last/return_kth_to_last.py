1e30f72ca60fb81b3f299c610c2f04edcdabfc08
César García <ceegee@outlook.com>
2025-10-29 19:11:37 -0700

    Add 2nd algorithm

--- 2.2 Return Kth to Last/return_kth_to_last.py
+++ 2.2 Return Kth to Last/return_kth_to_last.py
@@ -3,20 +3,22 @@
     Question 2.1 - Return Kth to Last
     Implement an algorithm to find the kth to last element of a single linked list.
 '''
-from LinkedList import LinkedList
+from LinkedList import LinkedList, Node
+error_empty_list = "List must have at least 1 element"
+error_list_boundaries = "Kth must be within the list boundaries"
 
 
-def return_Kth_to_last(linked_list: LinkedList, Kth: int) -> int:
+def return_Kth_to_last1(linked_list: LinkedList, Kth: int) -> int:
     """
+    Relies on the LinkedList class len() method.
     Time complexity: O(2*n) = O(n)
     Space complexity: O(1)
     """
     list_len = len(linked_list)
     if list_len == 0:
-        raise ValueError("List must have at least 1 element")
-
+        raise ValueError(error_empty_list)
     if Kth < 0 or list_len <= Kth:
-        raise ValueError("Kth must be within the list boundaries")
+        raise ValueError(error_list_boundaries)
 
     target = list_len - Kth - 1
 
@@ -27,3 +29,25 @@ def return_Kth_to_last(linked_list: LinkedList, Kth: int) -> int:
             return current.data
         count += 1
         current = current.next
+
+
+def return_Kth_to_last2(head: Node, Kth: int) -> int:
+    """
+    Does not rely on the LinkedList class.
+    Time complexity: O(n)
+    Space complexity: O(n)
+    """
+    items = list()
+    list_len = 0
+    current = head
+    while current:
+        items.append(current.data)
+        list_len += 1
+        current = current.next
+
+    if list_len == 0:
+        raise ValueError(error_empty_list)
+    if Kth < 0 or list_len <= Kth:
+        raise ValueError(error_list_boundaries)
+
+    return items[list_len - Kth - 1]
