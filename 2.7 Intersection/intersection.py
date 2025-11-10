5e1021bf2bea4de43a448beb1f0835c9d854e515
César García <ceegee@outlook.com>
2025-11-10 00:23:59 -0800

    Refactor intersection function and add areListsEqual tests

--- 2.7 Intersection/intersection.py
+++ 2.7 Intersection/intersection.py
@@ -7,13 +7,32 @@ Given two (singly) linked lists, determine if the two lists intersect. Return th
 intersection is defined based on reference, not value. That is, if the kth element of the first linked list is the exact
 same node (by reference) as the jth node of the second linked list, then they are intersecting.
 '''
-from LinkedList import LinkedList, Node
+from LinkedList import Node
+from typing import Optional
 
 
-def intersection(ll1: LinkedList, ll2: LinkedList) -> Node:
-    l1, l2 = ll1.to_list(), ll2.to_list()
-    for i1 in range(len(l1)):
-        for i2 in range(len(l2)):
-            if l1[i1:] == l2[i2:]:
-                return l1[i1]
+def intersection(head1: Optional[Node], head2: Optional[Node]) -> Optional[Node]:
+    cur1 = head1
+    while cur1:
+        cur2 = head2
+        while cur2:
+            if cur1.data == cur2.data:
+                if areListsEqual(cur1, cur2):
+                    return cur1
+            cur2 = cur2.next
+        cur1 = cur1.next
     return None
+
+
+def areListsEqual(node1: Optional[Node], node2: Optional[Node]) -> bool:
+    """
+    Checks if two linked lists are identical.
+    Time Complexity: O(n)
+    Space Complexity: O(1)
+    """
+    cur1, cur2 = node1, node2
+    while cur1 and cur2:
+        if cur1.data != cur2.data:
+            return False
+        cur1, cur2 = cur1.next, cur2.next
+    return cur1 is None and cur2 is None
--- 2.7 Intersection/test_intersection.py
+++ 2.7 Intersection/test_intersection.py
@@ -2,21 +2,24 @@ from intersection import intersection
 from LinkedList import LinkedList
 import pytest
 
-cases = [
+intersection_cases = [
     # list1, list2, expected
     # Positive cases
     ([1], [1], 1),
     ([1, 9], [2, 9], 9),
     ([1, 9, 3], [2, 9, 3], 9),
     ([1, 9, 2], [3, 4, 9, 2], 9),
-    ([3, 1, 5, 9, 7, 2, 1], [4, 6, 7, 2, 1], 7),
+    ([3, 1, 5, 9, 7, 2, 1], [4, 9, 6, 7, 2, 1], 7),
     # Negative cases
+    ([], [], None),
     ([1], [2], None),
     ([1], [1, 2], None),
     ([1, 9, 3], [2, 9, 3, 4], None),
 ]
 
 
-@pytest.mark.parametrize("list1, list2, expected", cases)
+@pytest.mark.parametrize("list1, list2, expected", intersection_cases)
 def test_intersection(list1, list2, expected):
-    assert intersection(LinkedList(list1), LinkedList(list2)) == expected
+    actual = intersection(LinkedList(list1).head, LinkedList(list2).head)
+    actual = actual.data if actual else None
+    assert actual == expected
