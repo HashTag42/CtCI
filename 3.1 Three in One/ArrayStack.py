c9e1762d4afef7be29254c6cc46abb2ce224bc87
César García <ceegee@outlook.com>
2025-11-13 17:19:34 -0800

    Refactor ArrayStack to support three stacks using a single array and update tests accordingly

--- 3.1 Three in One/ArrayStack.py
+++ 3.1 Three in One/ArrayStack.py
@@ -12,42 +12,77 @@ T = TypeVar("T")
 class ArrayStack(Generic[T]):
     """Uses an array to implement three stacks"""
 
-    # constructor
+    # Constructor
     def __init__(self,
+                 items0: Optional[List[T]] = None,
                  items1: Optional[List[T]] = None,
                  items2: Optional[List[T]] = None,
-                 items3: Optional[List[T]] = None,
                  ):
         """Initialize ArrayStack object. Optionally populates it with a list of items."""
         self.data = list()
-        self.data.append("stack1")
-        self.data.append("stack2")
-        self.data.append("stack3")
+        self.sizes = [0, 0, 0]
+
+        if items0:
+            for item in items0:
+                self.data.insert(self.sizes[0], item)
+            self.sizes[0] += len(items0)
         if items1:
             for item in items1:
-                self.data.insert(self.data.index("stack1") + 1, item)
+                self.data.insert(self.sizes[0] + self.sizes[1], item)
+            self.sizes[1] += len(items1)
         if items2:
             for item in items2:
-                self.data.insert(self.data.index("stack2") + 1, item)
-        if items3:
-            for item in items3:
-                self.data.insert(self.data.index("stack3") + 1, item)
+                self.data.insert(self.sizes[0] + self.sizes[1] + self.sizes[2], item)
+            self.sizes[2] += len(items2)
 
-    # interface
-    def push(self, stack, data: T) -> None:
-        """Add an item to the indicated stack"""
-        if stack not in ["stack1", "stack2", "stack3"]:
-            raise ValueError(f"Incorrect stack id: {stack}")
-        self.data.insert(self.data.index(stack) + 1, data)
+    # Public interface
+    def is_empty(self, stack_id) -> bool:
+        self._assert_stack_id(stack_id)
+        return False if self.sizes[stack_id] > 0 else True
 
-    def pop(self, stack) -> Optional[T]:
-        if stack not in ["stack1", "stack2", "stack3"]:
-            raise ValueError(f"Incorrect stack id: {stack}")
-        popped_value = self.data[self.data.index(stack)]
-        self.data.pop(self.data.index(stack) + 1)
+    def peek(self, stack_id) -> Optional[T]:
+        """Return the data at the top of the indicated stack"""
+        self._assert_stack_id(stack_id)
+        if self.sizes[stack_id] == 0:
+            raise IndexError("Nothing to peek at")
+        index = 0
+        for i in range(stack_id):
+            index += self.sizes[i]
+        return self.data[index]
+
+    def pop(self, stack_id) -> Optional[T]:
+        """Remove and return the data at the top of the indicated stack"""
+        self._assert_stack_id(stack_id)
+        if self.sizes[stack_id] == 0:
+            raise IndexError("Nothing to pop")
+        index = 0
+        for i in range(stack_id):
+            index += self.sizes[i]
+        popped_value = self.data[index]
+        self.data.pop(index)
+        self.sizes[stack_id] -= 1
         return popped_value
+
+    def push(self, stack_id, data: T) -> None:
+        """Add an item to the indicated stack"""
+        self._assert_stack_id(stack_id)
+        index = 0
+        for i in range(stack_id):
+            index += self.sizes[i]
+        self.data.insert(index, data)
+        self.sizes[stack_id] += 1
+
+    # Private methods
+    def _assert_stack_id(self, stack_id):
+        if 0 <= stack_id <= 2:
+            return True
+        else:
+            raise ValueError(f"Incorrect stack id: {stack_id}")
 
     # Dunder methods
     def __repr__(self) -> str:
         """Return a developer-friendly string representation"""
-        return f"ArrayStack({self.data})"
+        stack0 = list(self.data[0:self.sizes[0]])
+        stack1 = list(self.data[self.sizes[0]:self.sizes[0] + self.sizes[1]])
+        stack2 = list(self.data[self.sizes[0] + self.sizes[1]:self.sizes[0] + self.sizes[1] + self.sizes[2]])
+        return f"ArrayStack({stack0}, {stack1}, {stack2})"
--- 3.1 Three in One/test_ArrayStack.py
+++ 3.1 Three in One/test_ArrayStack.py
@@ -7,14 +7,15 @@ import pytest
 
 
 ################################################################################
-# region ArrayStack.__init__(items1, items2, items3)
-@pytest.mark.parametrize("items1, items2, items3, expected", [
-    ([], [], [], "ArrayStack(['stack1', 'stack2', 'stack3'])"),
-    ([10], [20], [30], "ArrayStack(['stack1', 10, 'stack2', 20, 'stack3', 30])"),
-    ([10, 11], [20, 21], [30, 31], "ArrayStack(['stack1', 11, 10, 'stack2', 21, 20, 'stack3', 31, 30])"),
+# region ArrayStack.__init__(items0, items1, items2)
+@pytest.mark.parametrize("items0, items1, items2, expected", [
+    ([], [], [], "ArrayStack([], [], [])"),
+    ([10], [20], [30], "ArrayStack([10], [20], [30])"),
+    ([], [20, 21], [], "ArrayStack([], [21, 20], [])"),
+    ([10, 11], [20, 21], [30, 31], "ArrayStack([11, 10], [21, 20], [31, 30])"),
 ])
-def test_ArrayStack__init__(items1, items2, items3, expected):
-    array_stack = ArrayStack(items1, items2, items3)
+def test_ArrayStack__init__(items0, items1, items2, expected):
+    array_stack = ArrayStack(items0, items1, items2)
     assert isinstance(array_stack, ArrayStack)
     assert repr(array_stack) == expected
 # endregion
@@ -22,14 +23,98 @@ def test_ArrayStack__init__(items1, items2, items3, expected):
 
 
 ################################################################################
-# region ArrayStack.push(stack, data)
+# region ArrayStack.is_empty(stack_id)
+@pytest.mark.parametrize("items1, items2, items3, stack_id, expected", [
+    ([], [], [], 0, True),
+    ([], [], [], 1, True),
+    ([], [], [], 2, True),
+    ([10], [20, 21], [30], 0, False),
+    ([10], [20, 21], [30], 1, False),
+    ([10], [20, 21], [30], 2, False),
+])
+def test_ArrayStack_is_empty(items1, items2, items3, stack_id, expected):
+    array_stack = ArrayStack(items1, items2, items3)
+    assert array_stack.is_empty(stack_id) == expected
+# endregion
+################################################################################
+
+
+################################################################################
+# region ArrayStack.peek(stack_id)
+@pytest.mark.parametrize("items0, items1, items2, stack_id, expected", [
+    ([10], [20], [30], 0, 10),
+    ([10], [20], [30], 1, 20),
+    ([10], [20], [30], 2, 30),
+    ([], [20, 21], [], 1, 21),
+    ([10, 11], [20, 21], [30, 31], 0, 11),
+    ([10, 11], [20, 21], [30, 31], 1, 21),
+    ([10, 11], [20, 21], [30, 31], 2, 31),
+])
+def test_ArrayStack_peek(items0, items1, items2, stack_id, expected):
+    array_stack = ArrayStack(items0, items1, items2)
+    assert array_stack.peek(stack_id) == expected
+
+
+@pytest.mark.parametrize("items0, items1, items2, stack_id, expected", [
+    ([], [], [], -1, ValueError),
+    ([], [], [], 3, ValueError),
+    ([], [], [], 0, IndexError),
+    ([], [], [], 1, IndexError),
+    ([], [], [], 2, IndexError),
+    ([], [20, 21], [], 0, IndexError),
+    ([], [20, 21], [], 0, IndexError),
+])
+def test_ArrayStack_peek_raises(items0, items1, items2, stack_id, expected):
+    array_stack = ArrayStack(items0, items1, items2)
+    with pytest.raises(expected):
+        array_stack.peek(stack_id)
+# endregion
+################################################################################
+
+
+################################################################################
+# region ArrayStack.pop(stack_id)
+@pytest.mark.parametrize("stack1, stack2, stack3, stack_id, popped, expected", [
+    ([10, 11], [20, 21], [30, 31], 0, 11,
+     "ArrayStack([10], [21, 20], [31, 30])"),
+    ([10, 11], [20, 21], [30, 31], 1, 21,
+     "ArrayStack([11, 10], [20], [31, 30])"),
+    ([10, 11], [20, 21], [30, 31], 2, 31,
+     "ArrayStack([11, 10], [21, 20], [30])"),
+])
+def test_ArrayStack_pop(stack1, stack2, stack3, stack_id, popped, expected):
+    array_stack = ArrayStack(stack1, stack2, stack3)
+    assert array_stack.pop(stack_id) == popped
+    assert repr(array_stack) == expected
+
+
+@pytest.mark.parametrize("items0, items1, items2, stack_id, expected", [
+    ([], [], [], -1, ValueError),
+    ([], [], [], 3, ValueError),
+    ([], [], [], 0, IndexError),
+    ([], [], [], 1, IndexError),
+    ([], [], [], 2, IndexError),
+    ([], [20, 21], [], 0, IndexError),
+    ([], [20, 21], [], 0, IndexError),
+])
+def test_ArrayStack_pop_raises(items0, items1, items2, stack_id, expected):
+    array_stack = ArrayStack(items0, items1, items2)
+    with pytest.raises(expected):
+        array_stack.pop(stack_id)
+
+# endregion
+################################################################################
+
+
+################################################################################
+# region ArrayStack.push(stack_id, data)
 @pytest.mark.parametrize("stack1, stack2, stack3, stack_id, data, expected", [
-    ([10, 11], [20, 21], [30, 31], "stack1", 12,
-     "ArrayStack(['stack1', 12, 11, 10, 'stack2', 21, 20, 'stack3', 31, 30])"),
-    ([10, 11], [20, 21], [30, 31], "stack2", 22,
-     "ArrayStack(['stack1', 11, 10, 'stack2', 22, 21, 20, 'stack3', 31, 30])"),
-    ([10, 11], [20, 21], [30, 31], "stack3", 32,
-     "ArrayStack(['stack1', 11, 10, 'stack2', 21, 20, 'stack3', 32, 31, 30])"),
+    ([10, 11], [20, 21], [30, 31], 0, 12,
+     "ArrayStack([12, 11, 10], [21, 20], [31, 30])"),
+    ([10, 11], [20, 21], [30, 31], 1, 22,
+     "ArrayStack([11, 10], [22, 21, 20], [31, 30])"),
+    ([10, 11], [20, 21], [30, 31], 2, 32,
+     "ArrayStack([11, 10], [21, 20], [32, 31, 30])"),
 ])
 def test_ArrayStack_push(stack1, stack2, stack3, stack_id, data, expected):
     array_stack = ArrayStack(stack1, stack2, stack3)
@@ -37,33 +122,56 @@ def test_ArrayStack_push(stack1, stack2, stack3, stack_id, data, expected):
     assert repr(array_stack) == expected
 
 
-def test_ArrayStack_push_raises():
+@pytest.mark.parametrize("stack_id", [
+    (-1),
+    (3),
+])
+def test_ArrayStack_push_raises(stack_id):
     array_stack = ArrayStack()
     with pytest.raises(ValueError):
-        array_stack.push("stack", 0)
+        array_stack.push(stack_id, 0)
 # endregion
 ################################################################################
 
 
 ################################################################################
-# region ArrayStack.pop(stack)
-@pytest.mark.parametrize("stack1, stack2, stack3, stack_id, expected", [
-    ([10, 11], [20, 21], [30, 31], "stack1",
-     "ArrayStack(['stack1', 10, 'stack2', 21, 20, 'stack3', 31, 30])"),
-    ([10, 11], [20, 21], [30, 31], "stack2",
-     "ArrayStack(['stack1', 11, 10, 'stack2', 20, 'stack3', 31, 30])"),
-    ([10, 11], [20, 21], [30, 31], "stack3",
-     "ArrayStack(['stack1', 11, 10, 'stack2', 21, 20, 'stack3', 30])"),
+# region ArrayStack._assert_stack_id(stack_id)
+@pytest.mark.parametrize("stack_id, expected", [
+    (0, True),
+    (1, True),
+    (2, True),
 ])
-def test_ArrayStack_pop(stack1, stack2, stack3, stack_id, expected):
-    array_stack = ArrayStack(stack1, stack2, stack3)
-    array_stack.pop(stack_id)
-    assert repr(array_stack) == expected
+def test_ArrayStack__assert_stack_id(stack_id, expected):
+    array_stack = ArrayStack()
+    assert array_stack._assert_stack_id(stack_id) == expected
 
 
-def test_ArrayStack_pop_raises():
+@pytest.mark.parametrize("stack_id", [
+    (-1),
+    (3),
+])
+def test_ArrayStack__assert_stack_id_raises(stack_id):
     array_stack = ArrayStack()
     with pytest.raises(ValueError):
-        array_stack.pop("stack")
+        array_stack._assert_stack_id(stack_id)
+# endregion
+################################################################################
+
+
+################################################################################
+# region ArrayStack.__repr__()
+@pytest.mark.parametrize("items1, items2, items3, expected", [
+    ([], [], [],
+     "ArrayStack([], [], [])"),
+    ([10], [20], [30],
+     "ArrayStack([10], [20], [30])"),
+    ([], [20, 21], [],
+     "ArrayStack([], [21, 20], [])"),
+    ([10, 11], [20, 21], [30, 31],
+     "ArrayStack([11, 10], [21, 20], [31, 30])"),
+])
+def test_ArrayStack__repr__(items1, items2, items3, expected):
+    array_stack = ArrayStack(items1, items2, items3)
+    assert repr(array_stack) == expected
 # endregion
 ################################################################################
