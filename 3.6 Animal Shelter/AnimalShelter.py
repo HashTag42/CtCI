b6bdbcbda95cd16371e921bcd5d09153857b60ad
César García <ceegee@outlook.com>
2025-11-19 16:29:11 -0800

    Add answer to question 3.6 Animal Shelter

---
+++ 3.6 Animal Shelter/AnimalShelter.py
@@ -0,0 +1,95 @@
+'''
+Cracking the Coding Interview - 6th Edition - https://www.crackingthecodinginterview.com/
+
+Question 3.6 Animal Shelter
+An animal shelter, which holds only dogs and cats, operates on a strictly "first in, first out basis". People must adopt
+either the "oldest" (based on arrival time) of all animals at the shelter, or they can select whether they would prefer
+a dog or a cat (and will receive the oldest animal of that type). They cannot select which specific animal they would
+like. Create the data structures to maintain this system and implement operations such as `enqueue`, `dequeueAny`,
+`dequeueDog`, and `dequeueCat`. You may use the built-in `LinkedList` data structure.
+
+LinkedList is implemented at https://github.com/HashTag42/Python/blob/master/Python101/LinkedList.py
+'''
+from LinkedList import LinkedList
+from typing import List, Optional
+from enum import Enum
+import time
+
+
+class AnimalType(Enum):
+    CAT = 1
+    DOG = 2
+
+
+class Animal():
+    def __init__(self, type: AnimalType, name: str) -> None:
+        self.type: AnimalType = type
+        self.time_admitted = time.time()
+        self.name: str = name
+
+    def __str__(self) -> str:
+        result: str = "("
+        match self.type:
+            case AnimalType.CAT:
+                result += "CAT"
+            case AnimalType.DOG:
+                result += "DOG"
+            case _:
+                pass
+        result += f":{self.name})"
+        return result
+
+
+class Cat(Animal):
+    def __init__(self, name: str) -> None:
+        super().__init__(AnimalType.CAT, name)
+
+
+class Dog(Animal):
+    def __init__(self, name: str) -> None:
+        super().__init__(AnimalType.DOG, name)
+
+
+class AnimalShelter(LinkedList):
+    ################################################################################
+    # region CONSTRUCTOR
+    def __init__(self, animals: Optional[List[Animal]] = None) -> None:
+        super().__init__(animals)
+    # endregion
+    ################################################################################
+
+    ################################################################################
+    # region PUBLIC INTERFACE
+    def enqueue(self, animal: Animal):
+        self.append(animal)
+
+    def dequeueAny(self) -> Animal:
+        return super().pop_head()
+
+    def dequeueCat(self) -> Cat:
+        return self._dequeueAnimal(AnimalType.CAT)
+
+    def dequeueDog(self) -> Dog:
+        return self._dequeueAnimal(AnimalType.DOG)
+    # endregion
+    ################################################################################
+
+    ################################################################################
+    # region PRIVATE INTERFACE
+    def _dequeueAnimal(self, animal_type: AnimalType) -> Animal:
+        previous_node = None
+        current_node = self.head
+        while current_node:
+            if current_node.data.type == animal_type:
+                if previous_node:
+                    previous_node.next = current_node.next
+                else:
+                    self.head = current_node.next
+                if current_node.next is None:
+                    self.tail = previous_node
+                return current_node.data
+            previous_node = current_node
+            current_node = current_node.next
+        return None
+    # endregion
+    ################################################################################
---
+++ 3.6 Animal Shelter/test_AnimalShelter.py
@@ -0,0 +1,109 @@
+from AnimalShelter import Animal, AnimalShelter, AnimalType, Cat, Dog
+import pytest
+
+
+################################################################################
+# region Animal
+def test_Animal__init():
+    animal = Animal(AnimalType.CAT, "FIFI")
+    assert isinstance(animal, Animal)
+
+
+@pytest.mark.parametrize("type, name, expected", [
+    (AnimalType.CAT, "Cat1", "(CAT:Cat1)"),
+    (AnimalType.DOG, "Dog1", "(DOG:Dog1)"),
+    (3, "Bird1", ""),
+])
+def test_Animal__str__(type, name, expected):
+    animal = Animal(type, name)
+    assert str(animal) == expected
+# endregion
+################################################################################
+
+
+################################################################################
+# region Cat
+def test_Cat__init__():
+    cat = Cat("FIFI")
+    assert isinstance(cat, Cat)
+
+
+def test_Cat__str__():
+    cat = Cat("FIFI")
+    assert str(cat) == "(CAT:FIFI)"
+# endregion
+################################################################################
+
+
+################################################################################
+# region Dog
+def test_Dog__init__():
+    dog = Dog("FIFI")
+    assert isinstance(dog, Dog)
+
+
+def test_Dog__str__():
+    dog = Dog("FIFI")
+    assert str(dog) == "(DOG:FIFI)"
+# endregion
+################################################################################
+
+
+################################################################################
+# region AnimalShelter
+def test_AnimalShelter__init__():
+    shelter = AnimalShelter()
+    assert isinstance(shelter, AnimalShelter)
+
+
+@pytest.mark.parametrize("animals, expected", [
+    ([], ""),
+    ([Cat("Cat1")], "(CAT:Cat1)"),
+    ([Cat("Cat1"), Dog("Dog1")], "(CAT:Cat1) -> (DOG:Dog1)"),
+    ([Cat("Cat1"), Dog("Dog1"), Dog("Dog2"), Cat("Cat2")], "(CAT:Cat1) -> (DOG:Dog1) -> (DOG:Dog2) -> (CAT:Cat2)"),
+])
+def test_AnimalShelter__init__with_animals(animals, expected):
+    shelter = AnimalShelter(animals)
+    assert str(shelter) == expected
+
+
+def test_AnimalShelter_enqueue():
+    shelter = AnimalShelter()
+    animals = [Cat("Cat1"), Dog("Dog1"), Dog("Dog2"), Cat("Cat2")]
+    for animal in animals:
+        shelter.enqueue(animal)
+    assert str(shelter) == "(CAT:Cat1) -> (DOG:Dog1) -> (DOG:Dog2) -> (CAT:Cat2)"
+
+
+def test_AnimalShelter_dequeueAny():
+    shelter = AnimalShelter([Cat("Cat1"), Dog("Dog1"), Dog("Dog2"), Cat("Cat2")])
+    shelter.dequeueAny()
+    assert str(shelter) == "(DOG:Dog1) -> (DOG:Dog2) -> (CAT:Cat2)"
+
+
+def test_AnimalShelter_dequeueCat():
+    shelter = AnimalShelter([Cat("Cat1"), Dog("Dog1"), Dog("Dog2"), Cat("Cat2")])
+    shelter.dequeueCat()
+    assert str(shelter) == "(DOG:Dog1) -> (DOG:Dog2) -> (CAT:Cat2)"
+
+
+def test_AnimalShelter_dequeueDog():
+    shelter = AnimalShelter([Cat("Cat1"), Dog("Dog1"), Dog("Dog2"), Cat("Cat2")])
+    shelter.dequeueDog()
+    assert str(shelter) == "(CAT:Cat1) -> (DOG:Dog2) -> (CAT:Cat2)"
+
+
+def test_AnimalShelter_dequeue_all():
+    shelter = AnimalShelter([Cat("Cat1"), Dog("Dog1"), Dog("Dog2"), Cat("Cat2")])
+    shelter.dequeueDog()
+    assert str(shelter) == "(CAT:Cat1) -> (DOG:Dog2) -> (CAT:Cat2)"
+    shelter.dequeueAny()
+    assert str(shelter) == "(DOG:Dog2) -> (CAT:Cat2)"
+    shelter.dequeueCat()
+    assert str(shelter) == "(DOG:Dog2)"
+    shelter.dequeueCat()
+    assert str(shelter) == "(DOG:Dog2)"
+    shelter.dequeueDog()
+    assert str(shelter) == ""
+# endregion
+################################################################################
